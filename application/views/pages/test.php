f6af43c997c882c22772da7cc29b59e362537996
Kyle Arch <kaarch@gmail.com>
2015-05-20 12:32:20 -0500

    test page needed "bb" prefix on node element

--- application/views/pages/test.php
+++ application/views/pages/test.php
@@ -5,7 +5,7 @@
 
   <div class="content">
     <h1>This is a test page</h1>
-    <div class="node" data-id="<?php echo $node->id; ?>" data-name="<?php echo $node->name; ?>" data-type="<?php echo $node->type; ?>">
+    <div class="bb-node" data-id="<?php echo $node->id; ?>" data-name="<?php echo $node->name; ?>" data-type="<?php echo $node->type; ?>">
       <?php echo $node->content; ?>
     </div>
   </div>
